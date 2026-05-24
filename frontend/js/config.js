2aedc3f9e7508108e627719590214ea75cdad4d9
Amritrajyadav17 <amritrajyadav06@gmail.com>
2026-05-24 22:54:08 +0530

    Update backend API URL

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -1,3 +1,3 @@
 // After backend deploy, change this to your Render/Railway API URL.
 // For local development keep: https://amrit-portfolio-91v1.onrender.com
-window.API_BASE_URL = "https://amrit-portfolio-91v1.onrender.com";
+window.API_BASE_URL = "https://amrit-portfolio-xhbh.onrender.com";
